27399f2c52bc443699ad2f1311d3a6772aa139ea
LOKESHWARAN-S07 <lokeshlogu077@gmail.com>
2024-09-23 22:11:20 +0530

    Add files via upload

---
+++ NONAME00.C
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int  a,b,c;
+clrscr();
+printf("enter the number");
+scanf("%d%d%d",&a,&b,&fc);
+if(a>c)
+{
+if(b>a)
+printf("b is grater");
+else
+printf("a is grater");
+}
+else
+{
+if(c>b)
+printf("c is grater");
+else
+printf("b is grater");
+}
+getch();
+}
+
+
+
+
+
+
+
